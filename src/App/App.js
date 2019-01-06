fc6e0ecdd9a0ecfddcf3b97f756be78a32159040
David Velarde <david.velarde.robles@mobgen.com>
2019-01-06 20:15:56 +0100

    Added logic for anual tax calculation

--- src/App/App.js
+++ src/App/App.js
@@ -35,30 +35,30 @@ class RowValue extends Component {
   render() {
     return (
       <div>
-      <ListItem onClick={this.handleOpen}>
-        <ListItemText primary={this.props.title} />
-        <ListItemText primary={this.props.amount} />
-      </ListItem>
-      <Dialog
-      onClose={this.handleClose}
-      aria-labelledby="customized-dialog-title"
-      open={this.state.open}
-    >
-    <MuiDialogTitle id="customized-dialog-title">
-        {this.props.label}
-      </MuiDialogTitle>
-      <MuiDialogContent>
-        <Typography gutterBottom>
-          {this.props.explanation}
-        </Typography>
-      </MuiDialogContent>
-      <MuiDialogActions>
-        <Button onClick={this.handleClose} color="primary">
-          Ok
+        <ListItem onClick={this.handleOpen}>
+          <ListItemText primary={this.props.title} />
+          <ListItemText primary={this.props.amount} />
+        </ListItem>
+        <Dialog
+          onClose={this.handleClose}
+          aria-labelledby="customized-dialog-title"
+          open={this.state.open}
+        >
+          <MuiDialogTitle id="customized-dialog-title">
+            {this.props.label}
+          </MuiDialogTitle>
+          <MuiDialogContent>
+            <Typography gutterBottom>
+              {this.props.explanation}
+            </Typography>
+          </MuiDialogContent>
+          <MuiDialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Ok
         </Button>
-      </MuiDialogActions>
-    </Dialog>
-    </div>
+          </MuiDialogActions>
+        </Dialog>
+      </div>
     )
   }
 }
@@ -72,6 +72,7 @@ class App extends Component {
       inputError: false
     }
     this.handleChange = this.handleChange.bind(this)
+    this.getRestOfRows = this.getRestOfRows.bind(this)
   }
 
   handleChange(e) {
@@ -83,7 +84,7 @@ class App extends Component {
       })
       return
     }
-    if(!isNaN(salary) && salary > 0) {
+    if (!isNaN(salary) && salary > 0) {
       this.setState({
         salary: salary,
         inputError: false
@@ -93,6 +94,87 @@ class App extends Component {
         inputError: true
       })
     }
+  }
+
+  getRestOfRows() {
+
+    let UIT = 4200
+    let essalud = 0.08
+
+    let salary = Number(this.state.salary)
+    let anualSalary = salary * 12
+    let grati = salary * 2 * (1 + essalud)
+    let vacaciones = salary
+    let cts = salary
+    let descuentoUIT = UIT * 7
+    var remuneracionBrutaAnual = anualSalary
+    remuneracionBrutaAnual+=grati
+    remuneracionBrutaAnual+=vacaciones
+    remuneracionBrutaAnual+=cts
+    let remuneracionNetaAnual = remuneracionBrutaAnual-descuentoUIT
+
+    var impuestoALaRenta = 0
+    var secciones = [0, 5, 20, 35, 45]
+    var porcentajes = [8, 14, 17, 20, 30]
+    var imposedSections = []
+
+    console.log("total = " + remuneracionNetaAnual)
+    for (var i = 0; i < porcentajes.length - 1; i++) {
+      var impuesto = 0
+      var min = secciones[i] * UIT
+      var max = secciones[i + 1] * UIT
+      var porcentaje = porcentajes[i] / 100  
+      console.log("min = " + min + " max = " + max + " porcentaje = " + porcentaje)
+      if (remuneracionNetaAnual > min && remuneracionNetaAnual <= max) {
+        impuesto = (remuneracionNetaAnual - min) * porcentaje
+        console.log("primer if " + (remuneracionNetaAnual - min) + " = " + (remuneracionNetaAnual - min) * porcentaje)
+      } else if (remuneracionNetaAnual > min) {
+        impuesto = ((max - min) * porcentaje)
+        console.log("segundo if " + (max - min) + " = " + ((max - min) * porcentaje))
+      }
+      if (impuesto == 0) {
+        break
+      }
+      imposedSections.push({
+        percentage: porcentajes[i],
+        chargedValue: impuesto
+      })
+      impuestoALaRenta += impuesto
+    }
+
+    let lastLimit = (secciones[secciones.length - 1] * UIT)
+    if (remuneracionNetaAnual > lastLimit) {
+      let lastPercentage = (porcentajes[porcentajes.length - 1] / 100)
+      let lastSection = (remuneracionNetaAnual - lastLimit) * lastPercentage
+      impuestoALaRenta += lastSection
+    }
+
+    let monthlyTax = (impuestoALaRenta/12)
+    let monthlySalary = salary - monthlyTax
+
+    var uiImposedSections = [] 
+    for(var i=0 ; i<imposedSections.length ; i++) {
+      let taxSection = imposedSections[i]
+      let title = "Segmento " + (i+1) + " - " + taxSection.percentage + "%"
+      uiImposedSections.push(<RowValue title={title} amount={taxSection.chargedValue} explanation="lalalalalla" />)
+    }
+
+    return (
+      <List component="nav">
+        <RowValue title="Sueldo mensual" amount={salary} explanation="lalalalalla" />
+        <RowValue title="Sueldo anual" amount={anualSalary} explanation="lalalalalla" />
+        <RowValue title="Gratificaciones" amount={grati} explanation="lalalalalla" />
+        <RowValue title="Vacaciones" amount={vacaciones} explanation="lalalalalla" />
+        <RowValue title="CTS" amount={cts} explanation="lalalalalla" />
+        <RowValue title="Remuneracion Bruta Anual" amount={remuneracionBrutaAnual} explanation="lalalalalla" />
+        <RowValue title="Descuento de 7 UITs" amount={descuentoUIT} explanation="lalalalalla" />
+        <RowValue title="Remuneracion Neta Anual" amount={remuneracionNetaAnual} explanation="lalalalalla" />
+        {uiImposedSections}
+        <RowValue title="Impuesto a la renta anual proyectado" amount={impuestoALaRenta} explanation="lalalalalla" />
+        <RowValue title="Impuesto a la renta mensual" amount={monthlyTax} explanation="lalalalalla" />
+        <RowValue title="Sueldo neto mensual" amount={monthlySalary} explanation="lalalalalla" />
+      </List>
+    )
   }
 
   render() {
@@ -114,10 +196,7 @@ class App extends Component {
             />
           </Grid>
           <Grid item xs={12}>
-            <List component="nav">
-              <RowValue title="Impuesto a la renta" amount="1000" explanation="lalalalalla" />
-            </List>
-
+              {this.getRestOfRows()}
           </Grid>
         </Grid>
       </div>
