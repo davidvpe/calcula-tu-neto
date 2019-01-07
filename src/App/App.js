6c2f1c9282381c581eb9919723c1ee4d94eb713d
David Velarde <david.velarde.robles@mobgen.com>
2019-01-07 11:05:31 +0100

    Modified UI to remove unnecessary info for not tax payers

--- src/App/App.js
+++ src/App/App.js
@@ -38,23 +38,31 @@ class RowValue extends Component {
   }
 
   render() {
+
+    var amountColor = "default"
+    if (this.props.amount < 0) {
+      amountColor = "error"
+    }
+
     return (
       <TableRow>
         <TableCell component="th" scope="row">
           {this.props.title}
-          <IconButton aria-label="Info">
+          <IconButton aria-label="Info" onClick={this.handleOpen}>
             <InfoIcon></InfoIcon>
           </IconButton>
         </TableCell>
         <TableCell align="right">
-          {this.props.amount}
+          <Typography color={amountColor}>
+            {"S/." + this.props.amount}
+          </Typography>
         </TableCell>
         <Dialog
           onClose={this.handleClose}
           aria-labelledby="customized-dialog-title"
           open={this.state.open}>
           <MuiDialogTitle id="customized-dialog-title">
-            {this.props.label}
+            {this.props.title}
           </MuiDialogTitle>
           <MuiDialogContent>
             <Typography gutterBottom>
@@ -87,13 +95,7 @@ class App extends Component {
   handleChange(e) {
     const salary = e.target.value
     console.log(salary)
-    if (salary.length === 0) {
-      this.setState({
-        inputError: false
-      })
-      return
-    }
-    if (!isNaN(salary) && salary > 0) {
+    if (!isNaN(salary)) {
       this.setState({
         salary: salary,
         inputError: false
@@ -115,38 +117,42 @@ class App extends Component {
     let grati = salary * 2 * (1 + essalud)
     let vacaciones = salary
     let cts = salary
-    let descuentoUIT = UIT * 7
+    var descuentoUIT = UIT * 7
     var remuneracionBrutaAnual = anualSalary
     remuneracionBrutaAnual += grati
     remuneracionBrutaAnual += vacaciones
     remuneracionBrutaAnual += cts
+
+    if (remuneracionBrutaAnual < descuentoUIT) {
+      descuentoUIT = remuneracionBrutaAnual
+    }
+
     let remuneracionNetaAnual = remuneracionBrutaAnual - descuentoUIT
+
 
     var impuestoALaRenta = 0
     var secciones = [0, 5, 20, 35, 45]
     var porcentajes = [8, 14, 17, 20, 30]
     var imposedSections = []
 
-    console.log("total = " + remuneracionNetaAnual)
     for (var i = 0; i < porcentajes.length - 1; i++) {
       var impuesto = 0
       var min = secciones[i] * UIT
       var max = secciones[i + 1] * UIT
       var porcentaje = porcentajes[i] / 100
-      console.log("min = " + min + " max = " + max + " porcentaje = " + porcentaje)
       if (remuneracionNetaAnual > min && remuneracionNetaAnual <= max) {
         impuesto = (remuneracionNetaAnual - min) * porcentaje
-        console.log("primer if " + (remuneracionNetaAnual - min) + " = " + (remuneracionNetaAnual - min) * porcentaje)
       } else if (remuneracionNetaAnual > min) {
         impuesto = ((max - min) * porcentaje)
-        console.log("segundo if " + (max - min) + " = " + ((max - min) * porcentaje))
       }
       if (impuesto == 0) {
         break
       }
       imposedSections.push({
         percentage: porcentajes[i],
-        chargedValue: impuesto
+        chargedValue: impuesto,
+        minSegment: secciones[i],
+        maxSegment: secciones[i + 1]
       })
       impuestoALaRenta += impuesto
     }
@@ -154,8 +160,15 @@ class App extends Component {
     let lastLimit = (secciones[secciones.length - 1] * UIT)
     if (remuneracionNetaAnual > lastLimit) {
       let lastPercentage = (porcentajes[porcentajes.length - 1] / 100)
+      console.log("lastPercentage" + lastPercentage)
       let lastSection = (remuneracionNetaAnual - lastLimit) * lastPercentage
+      console.log("lastSection" + lastSection)
       impuestoALaRenta += lastSection
+      imposedSections.push({
+        percentage: porcentajes[porcentajes.length - 1],
+        chargedValue: lastSection,
+        minSegment: secciones[secciones.length - 1]
+      })
     }
 
     let mandatoryInsuranceTax = 0.1
@@ -166,30 +179,44 @@ class App extends Component {
 
     let monthlyTax = (impuestoALaRenta / 12)
     let monthlySalary = salary - monthlyTax - afp
+    var uiImpuestoALaRenta = []
+    if (remuneracionNetaAnual > 0) {
+      uiImpuestoALaRenta.push(
+        <RowValue title="Sueldo anual" amount={anualSalary.toFixed(2)} explanation="Este es tu sueldo multiplicado por los 12 meses que esperas trabajar en el año" />,
+        <RowValue title="Gratificaciones" amount={grati.toFixed(2)} explanation="Este es el total de gratificaciones que vas a percibir incluyendo el 8% de ESSALUD" />,
+        <RowValue title="Vacaciones" amount={vacaciones.toFixed(2)} explanation="Este es el sueldo adicional por vacaciones" />,
+        <RowValue title="CTS" amount={cts.toFixed(2)} explanation="Este es el sueldo adicional por CTS" />,
+        <RowValue title="Remuneracion Bruta Anual" amount={remuneracionBrutaAnual.toFixed(2)} explanation="Esta es la suma de tu sueldo anual mas gratificaciones, vacaciones y CTS" />,
+        <RowValue title="Descuento de hasta 7 UITs" amount={-descuentoUIT.toFixed(2)} explanation="Este es un beneficio que otorga la SUNAT en el calculo del impuesto a la renta, te descuentan hasta 7 UITs" />,
+        <RowValue title="Remuneracion Neta Anual" amount={remuneracionNetaAnual.toFixed(2)} explanation="Esta es la remuneracion neta sobre la cual se calcula el impuesto a la renta con el beneficio del descuento de las 7 UITs" />
+      )
+      uiImpuestoALaRenta.push(
+        imposedSections.map(function (item, i) {
+          var title = ""
+          var explanation = ""
+          if (item.maxSegment) {
+            title = "Segmento " + (i + 1) + " (" + item.minSegment + " UIT - " + item.maxSegment + " UIT) - " + item.percentage + "%"
+            explanation = "Este es el segmento " + (i + 1) + ", por el cual debes pagar " + item.percentage + "% desde " + item.minSegment + " UITs hasta " + item.maxSegment + " UITs"
+          } else {
+            title = "Segmento " + (i + 1) + " (" + item.minSegment + " UIT o más) - " + item.percentage + "%"
+            explanation = "Este es el segmento " + (i + 1) + ", por el cual debes pagar " + item.percentage + "% desde " + item.minSegment + " UITs en adelante"
+          }
 
-    var uiImposedSections = []
-    for (var i = 0; i < imposedSections.length; i++) {
-      let taxSection = imposedSections[i]
-      let title = "Segmento " + (i + 1) + " - " + taxSection.percentage + "%"
-      uiImposedSections.push(<RowValue title={title} amount={taxSection.chargedValue} explanation="lalalalalla" />)
+          return <RowValue title={title} amount={-item.chargedValue.toFixed(2)} explanation={explanation} />
+        })
+      )
+      uiImpuestoALaRenta.push(
+        <RowValue title="Impuesto a la renta anual proyectado" amount={-impuestoALaRenta.toFixed(2)} explanation="Este es el total de impuesto a la renta que debes pagar en el año" />,
+        <RowValue title="Impuesto a la renta mensual" amount={-monthlyTax.toFixed(2)} explanation="Este seria el monton mensual que se descuenta por impuesto a la renta" />)
     }
 
     return (
       <Table className="Table">
         <TableBody>
-          <RowValue title="Sueldo mensual" amount={salary} explanation="lalalalalla" />
-          <RowValue title="Sueldo anual" amount={anualSalary} explanation="lalalalalla" />
-          <RowValue title="Gratificaciones" amount={grati} explanation="lalalalalla" />
-          <RowValue title="Vacaciones" amount={vacaciones} explanation="lalalalalla" />
-          <RowValue title="CTS" amount={cts} explanation="lalalalalla" />
-          <RowValue title="Remuneracion Bruta Anual" amount={remuneracionBrutaAnual} explanation="lalalalalla" />
-          <RowValue title="Descuento de 7 UITs" amount={descuentoUIT} explanation="lalalalalla" />
-          <RowValue title="Remuneracion Neta Anual" amount={remuneracionNetaAnual} explanation="lalalalalla" />
-          {uiImposedSections}
-          <RowValue title="Impuesto a la renta anual proyectado" amount={impuestoALaRenta} explanation="lalalalalla" />
-          <RowValue title="Impuesto a la renta mensual" amount={monthlyTax} explanation="lalalalalla" />
-          <RowValue title="Aporte a AFP" amount={afp} explanation="lalalalalla" />
-          <RowValue title="Sueldo neto mensual" amount={monthlySalary} explanation="lalalalalla" />
+          <RowValue title="Sueldo mensual" amount={salary.toFixed(2)} explanation="Este es tu sueldo mensual" />
+          {uiImpuestoALaRenta}
+          <RowValue title="Aporte a AFP" amount={-afp.toFixed(2)} explanation="Esto es un aproximado de lo pagarias por AFP" />
+          <RowValue title="Sueldo neto mensual" amount={monthlySalary.toFixed(2)} explanation="Este es tu sueldo neto mensual" />
         </TableBody>
       </Table>
     )
